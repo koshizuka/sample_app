2ae58e941ed5d5409e68b8b21f3e266270961b81
koshizuka <koshizuka@akibare.net>
2015-10-20 11:51:17 +0900

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe User do
 	
-  before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
+  before { @user = User.new(name: "Example User", email: "user@example.com",
+  													password: "foobar", password_confirmation: "foobar") }
 
   subject { @user }
 
@@ -11,6 +12,8 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -60,14 +63,10 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "when password is not present" do
-	  before do
-	    @user = User.new(name: "Example User", email: "user@example.com",
-	                     password: " ", password_confirmation: " ")
-	  end
-
-		it { should_not be_valid }
- 	end
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
 
  	describe "when password doesn't match confirmation" do
 	  before { @user.password_confirmation = "mismatch" }
@@ -94,5 +93,10 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+
+    describe "remember token" do
+	    before { @user.save }
+	    its(:remember_token) { should_not be_blank }
+  	end
   end
 end
